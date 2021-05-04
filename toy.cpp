b8c5a86769c2e9c45323085349bb08f17f6cca20
Emily Howell <emilythowell@gmail.com>
2021-05-04 12:02:17 -0700

    Added some commenting and made the code a little cleaner

--- toy.cpp
+++ toy.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 
-std::vector<int> parseInputString(std::string line) {
-    std::vector <int> elements;
+void parseInputString(std::vector<int> &elements, std::string line) {
     int value = 0;
 
+    // loop through the given string and split apart the numbers
     for (int i = 0; i < line.size(); i++) {
-        if(line[i] == ' ') {
+        if(isdigit(line[i])){
+            value = value*10 + (line[i]-'0');
+        }
+        else {
             elements.push_back(value);
             value = 0;
         }
-        else if(isdigit(line[i])){
-            value = value*10 + (line[i]-'0');
-        }
     }
-    elements.push_back(value);
-
-    return elements;
+    elements.push_back(value); // last value
 }
 
 void swap(std::vector<int> &elements, int index) { 
+    //swapping by removing the j+1 element and inserting it back in at index j
     int temp = elements.at(index + 1);
     elements.erase(elements.begin()+ index + 1);
     elements.insert(elements.begin()+index, temp); 
@@ -29,21 +28,28 @@ void bubbleSort(std::vector<int> &elements, int n)
 { 
     int i, j; 
     for (i = 0; i < n-1; i++){
-        // Last i elements are already in place 
-        for (j = 0; j < n-i-1; j++) 
-            if (elements.at(j) > elements.at(j+1)) 
+        // Last i elements are already in place, check the j and j+1 element
+        for (j = 0; j < n-i-1; j++) {
+            if (elements.at(j) > elements.at(j+1)) {
                 swap(elements, j); 
+            }
+        }
     }
 }
 
 int main(){
+    // Get input string of values to be sorted seperated by spaces
     std::string line;
-    std::cout << "Enter the array to be sorted delimited by spaces" << std::endl;
+    std::cout << "Enter the elements to be sorted delimited by spaces:" << std::endl;
     getline(std::cin,line);
-    std::vector<int> elements = parseInputString(line);
-    bubbleSort(elements, elements.size());
+
+    std::vector<int> elements;
+    parseInputString(elements, line); // Parse the string into the vector
+    bubbleSort(elements, elements.size()); // Sort the vector
     
-    for (int i = 0; i < elements.size(); i++){
+    // Output the final product
+    std::cout << std::endl << "Sorted elements:" << std::endl;
+    for (int i = 0; i < elements.size(); i++) {
         std::cout << elements.at(i) << " ";
     }
     std::cout << std::endl;
